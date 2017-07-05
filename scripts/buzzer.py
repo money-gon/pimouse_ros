0e79e23e6fed3721226f863a7f7c7f2c418e4864
money-gon <*money-gon@todo.todo>
2017-07-05 22:27:08 +0900

    Update buzzer.py

--- scripts/buzzer.py
+++ scripts/buzzer.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
+
 import rospy
-rospy.init_node('buzzer')
-rospy.spin()
+from std_msgs.msg import UInt16
+
+def recv_buzzer(data):
+    rospy.loginfo(type(data))
+    rospy.loginfo(data.data)
+
+if __name__ == '__main__':
+    rospy.init_node('buzzer')
+    rospy.Subscriber("buzzer", UInt16, recv_buzzer)
+    rospy.spin()
