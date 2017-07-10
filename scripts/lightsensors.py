33a54b1f20d094f99cd60eabb5b57ed65a563b46
money-gon <*money-gon@todo.todo>
2017-07-10 22:57:52 +0900

    Add lightsensors test files

--- scripts/lightsensors.py
+++ scripts/lightsensors.py
@@ -4,12 +4,24 @@
 import sys, rospy
 from pimouse_ros.msg import LightSensorValues
 
+def get_freq():
+    f = rospy.get_param('lightsensors_freq', 10)
+    try:
+        if f <= 0.0:
+            raise Exception()
+    except:
+        rospy.logerr("value error: lightsensors_freq")
+        sys.exit(1)
+
+    return f
+
 if __name__ == '__main__':
     devfile = '/dev/rtlightsensor0'
     rospy.init_node('lightsensors')
     pub = rospy.Publisher('lightsensors', LightSensorValues, queue_size=1)
 
-    rate = rospy.Rate(10)
+    freq = get_freq()
+    rate = rospy.Rate(freq)
     while not rospy.is_shutdown():
         try:
             with open(devfile, 'r') as f:
@@ -25,5 +37,10 @@ if __name__ == '__main__':
                 pub.publish(d)
         except IOError:
             rospy.logerr("cannot write to " + devfile)
+
+        f = get_freq()
+        if f != freq:
+            freq = f
+            rate = rospy.Rate(freq)
 
         rate.sleep()
